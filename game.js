a9663462be598a8ffc4f3a75e81e7327dda7e233
zanelkt <zanelockett@gmail.com>
2018-07-12 11:01:52 +1200

    blue green clicks sprint 5.4.1

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
   }
 }
 
@@ -22,7 +23,10 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
