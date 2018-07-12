97e6b672e8a1f4c4fc0301921348c9f2aff9062a
zanelkt <zanelockett@gmail.com>
2018-07-12 12:42:22 +1200

    sprint 5.4 finished

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dbclick', hide)
   }
 }
 
@@ -28,6 +29,11 @@ function makeBlue (evt) {
   updateCounts()
 }
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classlist.toggle('invisible')
+  updateCounts()
+}
+
 
 function updateCounts () {
   var totals = {
@@ -37,6 +43,19 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var dots = document.getElementsByClassName('board')[0].children
+
+  for (var i = 0; i < dots.length; i++)  {
+    if (dots[i].classList.contains('blue')) {
+      totals.blue++;
+    }
+      else if (dots[i].classList.contains('green')) {
+        totals.green++;
+      }
+        else if (dots[i].classList.contains('invisible'))  {
+          totals.invisible++;
+        }
+  } 
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
